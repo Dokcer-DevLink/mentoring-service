b3f429b8ce30e0eb34cd32a32ff8abeef8130a59
mgKang3646 <lordofkangs@naver.com>
2023-12-26 13:55:31 +0900

    feat: 서비스간 통신 인터페이스 추가

---
+++ src/main/java/com/goorm/devlink/mentoringservice/feign/ProfileServiceClient.java
@@ -0,0 +1,21 @@
+package com.goorm.devlink.mentoringservice.feign;
+
+import com.goorm.devlink.mentoringservice.vo.request.ScheduleCreateRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "profile-service")
+public interface ProfileServiceClient {
+
+    /** 스케줄 생성 **/
+    @PostMapping("/api/myprofile/schedule")
+    public ResponseEntity<Void> setUserCalendarSchedule(@RequestHeader("userUuid") String userUuid,
+                                                        @RequestBody ScheduleCreateRequest scheduleCreateRequest);
+
+    /** 스케줄 취소 **/
+    @DeleteMapping("/api/myprofile/schedule")
+    public ResponseEntity<Void> deleteUserCalendarSchedule(@RequestHeader("userUuid") String userUuid,
+                                                           @RequestParam("mentoringUuid") String mentoringUuid);
+
+}
